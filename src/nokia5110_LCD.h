f74bd00a4a97c1582aa2b9f49e87bd78bbde1ffc
Alwin Leerling <dna.leerling@gmail.com>
2021-01-25 14:54:55 +1300

    Added configuration file
    With the new configuration file a user can now indicate which
    MCU family is used. With this information the library will pull in
    the correct HAL header file.

--- src/nokia5110_LCD.h
+++ src/nokia5110_LCD.h
@@ -53,7 +53,30 @@
 
 #include <stdbool.h>
 #include "font.h"
+
+#include "nokia5110_conf.h"
+
+#if defined(STM32F0)
+#include "stm32f0xx_hal.h"
+#elif defined(STM32F1)
+#include "stm32f1xx_hal.h"
+#elif defined(STM32F4)
 #include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
+#elif defined(STM32L0)
+#include "stm32l0xx_hal.h"
+#elif defined(STM32L4)
+#include "stm32l4xx_hal.h"
+#elif defined(STM32F3)
+#include "stm32f3xx_hal.h"
+#elif defined(STM32H7)
+#include "stm32h7xx_hal.h"
+#elif defined(STM32F7)
+#include "stm32f7xx_hal.h"
+#else
+#error "nokia5110 library was tested only on  STM32F0, STM32F1, STM32F3, STM32F4, STM32F7, STM32L0, STM32L4, STM32H7 MCU families. Please modify nokia5110_LCD.h if you know what you are doing. Also please send a pull request if it turns out the library works on other MCU's as well!"
+#endif
+
 
 #define LCD_COMMAND 0
 #define LCD_DATA 1
